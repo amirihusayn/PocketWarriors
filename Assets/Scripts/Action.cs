92e3c02b05749283f6c577928ab16a68e870c1c8
Amir Goodarzi <amirgoodarzi75@gmail.com>
2020-12-06 00:49:07 +0330

    Initial warrior tests

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+    [SerializeField] private Animator warriorAnimator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+            warriorAnimator.SetTrigger("OnRightVerticalHit");  
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+            warriorAnimator.SetTrigger("OnRightHorizontalHit");  
+    }
+}
