54cb00412538450f7b3c0b0c2940d66bbb512f6f
ihusayn <amirgoodarzi75@gmail.com>
2021-07-22 12:38:31 +0430

    Add CameraForward.cs

---
+++ Assets/CameraForward.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraForward : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private bool isCameraTransformInitialized;
+
+    private void Start() 
+    {
+        StopAllCoroutines();
+        StartCoroutine("GetCameraTransform");
+    }
+
+    private void LateUpdate() 
+    {
+        if(isCameraTransformInitialized)
+            gameObject.transform.forward = cameraTransform.forward;
+    }
+
+    private IEnumerator GetCameraTransform()
+    {
+        yield return new WaitForSeconds(3f);
+        cameraTransform = Camera.main.gameObject.transform;
+        isCameraTransformInitialized = true;
+    }
+}
